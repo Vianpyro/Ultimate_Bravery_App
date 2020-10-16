8928dcd19d11da7c987745f38cdc60d56efeedd6
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2020-10-16 16:32:50 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,398 @@
+ROLES = [
+    "Toplane",
+    "Jungle",
+    "Midlane",
+    "Botlane",
+    "Support"
+]
+
+SUMMONER_SPELLS = [
+    "Heal",
+    "Ghost",
+    "Barrier",
+    "Exhaust",
+    "Flash",
+    "Teleport",
+    "Smite",
+    "Cleanse",
+    "Ignite"
+]
+
+SUPPORT_ITEMS = [
+    "Spellthief’s Edge",
+    "Steel Shoulderguards",
+    "Relic Shield",
+    "Spectral Sickle"
+]
+
+FINISHED_ITEMS = [
+    "Abyssal Mask",
+    "Adaptive Helm",
+    "Archangel's Staff",
+    "Ardent Censer",
+    "Athene's Unholy Grail",
+    "Banshee's Veil",
+    "Black Cleaver",
+    "Blade of the Ruined King",
+    "Bloodthirster",
+    "Dead Man's Plate",
+    "Death's Dance",
+    "Duskblade of Draktharr",
+    "Edge of Night",
+    "Essence Reaver",
+    "Frozen Heart",
+    "Frozen Mallet",
+    "Gargoyle Stoneplate",
+    "Guardian Angel",
+    "Guinsoo",
+    "Hextech GLP-800",
+    "Hextech Gunblade",
+    "Hextech Protobelt-01",
+    "Iceborn Gauntlet",
+    "Infinity Edge",
+    "Knight's Vow",
+    "Liandry's Torment",
+    "Lich Bane",
+    "Locket of the Iron Solari",
+    "Lord Dominik's Regards",
+    "Luden's Echo",
+    "Manamune",
+    "Maw of Malmortius",
+    "Mejai's Soulstealer",
+    "Mercurial Scimitar",
+    "Mikael's Crucible",
+    "Morellonomicon",
+    "Mortal Reminder",
+    "Muramana",
+    "Nashor's Tooth",
+    "Phantom Dancer",
+    "Rabadon's Deathcap",
+    "Randuin's Omen",
+    "Rapid Firecannon",
+    "Ravenous Hydra",
+    "Redemption",
+    "Righteous Glory",
+    "Rod of Ages",
+    "Runaan's Hurricane",
+    "Rylai's Crystal Scepter",
+    "Sanguine Blade",
+    "Shurelya's Reverie",
+    "Spellbinder",
+    "Spirit Visage",
+    "Statikk Shiv",
+    "Sterak's Cage",
+    "Stormrazor",
+    "Sunfire Cape",
+    "Thornmail",
+    "Titanic Hydra",
+    "Trinity Force",
+    "Twin Shadows",
+    "Umbral Glaive",
+    "Void Staff",
+    "Warmog's Armor",
+    "Wit's End",
+    "Youmuu's Ghostblade",
+    "Zeke's Convergence",
+    "Zhonya's Hourglass"
+]
+
+BOOTS = [
+    "Berserker's Greaves",
+    "Boots of Mobility",
+    "Boots of Swiftness",
+    "Ninja Tabi",
+    "Boots of Mobility",
+    "Ionian Boots of Lucidity",
+    "Mercury's Treads",
+    "Sorcerer's Shoes"
+]
+
+CHAMPIONS = [
+    "Aatrox",
+    "Ahri",
+    "Akali",
+    "Alistar",
+    "Amumu",
+    "Anivia",
+    "Annie",
+    "Ashe",
+    "Azir",
+    "Blitzcrank",
+    "Brand",
+    "Braum",
+    "Caitlyn",
+    "Camille",
+    "Cassiopeia",
+    "Chogath",
+    "Corki",
+    "Darius",
+    "Diana",
+    "Dr. mundo",
+    "Draven",
+    "Elise",
+    "Evelynn",
+    "Ezreal",
+    "Fiddlesticks",
+    "Fiora",
+    "Fizz",
+    "Galio",
+    "Gangplank",
+    "Garen",
+    "Gnar",
+    "Gragas",
+    "Graves",
+    "Hecarim",
+    "Heimerdinger",
+    "Irelia",
+    "Janna",
+    "Jarvan iv",
+    "Jax",
+    "Jayce",
+    "Jinx",
+    "Kalista",
+    "Karma",
+    "Karthus",
+    "Kassadin",
+    "Katarina",
+    "Kayle",
+    "Kennen",
+    "Khazix",
+    "Kogmaw",
+    "Leblanc",
+    "Lee sin",
+    "Leona",
+    "Lissandra",
+    "Lucian",
+    "Lulu",
+    "Lux",
+    "Malphite",
+    "Malzahar",
+    "Maokai",
+    "Master yi",
+    "Miss fortune",
+    "Mordekaiser",
+    "Morgana",
+    "Nami",
+    "Nasus",
+    "Nautilus",
+    "Nidalee",
+    "Nocturn",
+    "Nunu",
+    "Olaf",
+    "Orianna",
+    "Pantheon",
+    "Poppy",
+    "Quinn",
+    "Rammus",
+    "Reksai",
+    "Renekton",
+    "Rengar",
+    "Riven",
+    "Rumble",
+    "Ryze",
+    "Samira",
+    "Sejuani",
+    "Seraphine",
+    "Shaco",
+    "Shen",
+    "Shyvana",
+    "Singed",
+    "Sion",
+    "Sivir",
+    "Skarner",
+    "Sona",
+    "Soraka",
+    "Swain",
+    "Sylas",
+    "Syndra",
+    "Taliyah",
+    "Talon",
+    "Taric",
+    "Teemo",
+    "Thresh",
+    "Tristana",
+    "Trundle",
+    "Tryndamere",
+    "Twisted fate",
+    "Twitch",
+    "Udyr",
+    "Urgot",
+    "Varus",
+    "Vayne",
+    "Veigar",
+    "Velkoz",
+    "Vi",
+    "Viktor",
+    "Vladimir",
+    "Volibear",
+    "Warwick",
+    "Wukong",
+    "Xerath",
+    "Xin zhao",
+    "Yasuo",
+    "Yorick",
+    "Zac",
+    "Zed",
+    "Ziggs",
+    "Zilean",
+    "Zyra",
+    "Aphelios",
+    "Aurelion sol",
+    "Bard",
+    "Ekko",
+    "Illaoi",
+    "Ivern",
+    "Jhin",
+    "Kaisa",
+    "Kayne",
+    "Kindred",
+    "Kled",
+    "Lillia",
+    "Neeko",
+    "Ornn",
+    "Pyke",
+    "Qiyana",
+    "Rakan",
+    "Senna",
+    "Sett",
+    "Tahm kench",
+    "Xayah",
+    "Yone",
+    "Yuumi",
+    "Zoe"
+]
+
+RUNES = [
+    {
+        "Precision": [
+            [
+                "Press the Attack",
+                "Lethal Tempo",
+                "Fleet Footwork",
+                "Conqueror"
+            ],
+            [
+                "Overheal",
+                "Triumph",
+                "Presence of Mind"
+            ],
+            [
+                "Legend: Alacrity",
+                "Legend: Tenacity",
+                "Legend: Bloodline"
+            ],
+            [
+                "Coup de Grave",
+                "Cut Down",
+                "Last Stand"
+            ]
+        ],
+        "Domination": [
+            [
+                "Electrocute",
+                "Predator",
+                "Dark Harvest",
+                "Hail of Blades"
+            ],
+            [
+                "Cheap Shot",
+                "Taste of Blood",
+                "Sudden Impact"
+            ],
+            [
+                "Zombie Ward",
+                "Ghost Poro",
+                "Eyeball Collection"
+            ],
+            [
+                "Ravenous Hunter",
+                "Ingenious Hunter",
+                "Rentless Hunter",
+                "Ultimate Hunter"
+            ]
+        ],
+        "Sorcery": [
+            [
+                "Summon Aery",
+                "Arcane Comet",
+                "Phase Rush"
+            ],
+            [
+                "Nullifying Orb",
+                "Manaflow Band",
+                "Nimbus Cloak"
+            ],
+            [
+                "Transcendence",
+                "Celerity",
+                "Absolute Focus"
+            ],
+            [
+                "Scorch",
+                "Waterwalking",
+                "Gathering Storm"
+            ]
+        ],
+        "Resolve": [
+            [
+                "Grasp of the Undying",
+                "Aftershock",
+                "Guardian"
+            ],
+            [
+                "Demolish",
+                "Font of Life",
+                "Shield Bash"
+            ],
+            [
+                "Conditioning",
+                "Second Wind",
+                "Bone Plating"
+            ],
+            [
+                "Overgrowth",
+                "Revitalise",
+                "Unflinching"
+            ]
+        ],
+        "Inspiration": [
+            [
+                "Glacial Augment",
+                "Unsealed Spellbook",
+                "Prototype: Omnistone"
+            ],
+            [
+                "Hextech Flashtraption",
+                "Magical Footwear",
+                "Perfect Timing"
+            ],
+            [
+                "Future's Market",
+                "Minion Dematerializer",
+                "Biscuit Delivery"
+            ],
+            [
+                "Cosmic Insight",
+                "Approach Velocity",
+                "Time Warp Tonic"
+            ]
+        ]
+    },
+    {
+        "Slot1": [
+            "Option1: Bonus Adaptive Damage",
+            "Option2: Bonus Attack Speed",
+            "Option3: Cooldown Reduction"
+        ],
+        "Slot2": [
+            "Option1: Bonus Adaptive Damage",
+            "Option2: Bonus Armor",
+            "Option3: Bonus Magic Resistance"
+        ],
+        "Slot3": [
+            "Option1: Bonus Health",
+            "Option2: Bonus Armor",
+            "Option3: Bonus Magic"
+        ]
+    }
+]
---
+++ main.py
@@ -0,0 +1,95 @@
+# -*- coding = utf-8 -*-
+
+from random import randint
+from data import *
+
+champion = CHAMPIONS[randint(0, len(CHAMPIONS) - 1)]
+role = ROLES[randint(0, len(ROLES) - 1)]
+boots = BOOTS[randint(0, len(BOOTS) - 1)]
+spells = ['QWE'[randint(0, 2)]]
+
+#################################
+#   RUNES
+#################################
+
+runes_pages = [path for path in RUNES[0]]
+pre_runes = [randint(0, len(runes_pages) - 1) for _ in range(2)]
+while pre_runes[0] == pre_runes[1]:
+    pre_runes[1] = randint(0, len(runes_pages) - 1)
+
+runes_paths = [
+    RUNES[0][runes_pages[pre_runes[0]]],
+    RUNES[0][runes_pages[pre_runes[1]]]
+]
+
+runes = [
+    [
+        [runes_paths[0][i][randint(0, len(runes_paths[0][i]) - 1)] for i in range(4)],
+        [runes_paths[1][j + 1 + (pre_runes[0] + pre_runes[1]) // 4]
+            [randint(0, len(runes_paths[1][j + 1 + (pre_runes[0] + pre_runes[1]) // 4]) - 1)] for j in range(2)]
+    ],
+    [RUNES[1][f"Slot{slot + 1}"][randint(0, 2)] for slot in range(3)]
+]
+
+#################################
+#   BUILD
+#################################
+
+len_finished_items = len(FINISHED_ITEMS)
+
+if role == "Support": build = [SUPPORT_ITEMS[randint(0, len(SUPPORT_ITEMS) - 1)]]
+elif role == "Jungle":
+    core_items = ["Skirmisher's Sabre", "Stalker's Blade"]
+    enchant_items = ["Bloodrazor", "Cinderhulk", "Runic Echoes", "Warrior"]
+    build = [f"{core_items[randint(0, len(core_items) - 1)]} ({enchant_items[randint(0, len(enchant_items) - 1)]})"]
+else:
+    build = [FINISHED_ITEMS[randint(0, len_finished_items - 1)]]
+
+while len(build) < 5:
+    new_item = FINISHED_ITEMS[randint(0, len_finished_items - 1)]
+    if not new_item in build:
+        build.append(new_item)
+
+#################################
+#   SUMMONER SPELLS
+#################################
+
+len_summoner_spells = len(SUMMONER_SPELLS)
+
+if role == "Jungle":
+    sums = ["Smite"]
+else: sums = [SUMMONER_SPELLS[randint(0, len_summoner_spells - 1)]]
+
+while len(sums) < 2:
+    new_sums = SUMMONER_SPELLS[randint(0, len_summoner_spells - 1)]
+    if not new_sums in sums:
+        sums.append(new_sums)
+
+#################################
+#   SPELLS ORDER
+#################################
+
+while len(spells) < 3:
+    new_spell = 'QWE'[randint(0, 2)]
+    if not new_spell in spells:
+        spells.append(new_spell)
+
+#################################
+#   DISPLAY
+#################################
+
+print(f"""
+ROLE: {role}
+CHAMPION: {champion}
+SUMMONERS: {sums[0]}, {sums[1]}
+RUNES: ({runes_pages[pre_runes[0]]}, {runes_pages[pre_runes[1]]})
+\t- {runes[0][0][0]} : {runes[0][0][1]} - {runes[0][0][2]} - {runes[0][0][3]}
+\t- {runes[0][1][0]} - {runes[0][1][1]}
+\t- {runes[1][0]} - {runes[1][1]} - {runes[1][2]}
+
+SPELLS: {spells[0]} > {spells[1]} > {spells[2]}
+BUILD: {build}
+BOOTS: {boots}
+""")
+
+input("Good luck, have fun !")
