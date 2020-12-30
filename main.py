8d34823cdf1ea1cc091057769c340f499987a9d8
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2020-12-30 12:16:33 -0500

    Remade the whole program

--- main.py
+++ main.py
@@ -1,95 +1,115 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+from lxml import html
 from random import randint
-from data import *
+import requests, json
 
-champion = CHAMPIONS[randint(0, len(CHAMPIONS) - 1)]
-role = ROLES[randint(0, len(ROLES) - 1)]
-boots = BOOTS[randint(0, len(BOOTS) - 1)]
-spells = ['QWE'[randint(0, 2)]]
+class Scraper:
+    def __init__(self):
+        '''
+        Since League of Legends is developed by Riot Games
+        This program obtains its data from Riot Games services.
+        '''
+        self.base_url               = 'https://ddragon.leagueoflegends.com'
+        self.patch                  = requests.get(f'{self.base_url}/api/versions.json').json()[0]
+        self.champions_json         = requests.get(f'{self.base_url}/cdn/{self.patch}/data/en_US/champion.json').json()
+        self.items_json             = requests.get(f'{self.base_url}/cdn/{self.patch}/data/en_US/item.json').json()
+        self.summoner_spells_json   = requests.get(f'{self.base_url}/cdn/{self.patch}/data/en_US/summoner.json').json()
+        self.runes_json             = requests.get(f'{self.base_url}/cdn/{self.patch}/data/en_US/runesReforged.json').json()
 
-#################################
-#   RUNES
-#################################
+        self.champions              = [champion for champion in self.champions_json['data']]
+        self.items                  = self.items_json['data']
+        self.positions              = ['Top', 'Jungle', 'Middle', 'Bottom (ADC/APC)', 'Support']
+        self.summoner_spells        = [self.summoner_spells_json['data'][e]['name'] for e in self.summoner_spells_json['data'] if 'CLASSIC' in self.summoner_spells_json['data'][e]['modes']]
+        self.runes = [
+            [
+                [
+                    self.runes_json[i]['slots'][j]['runes'][k]['key']
+                    for k in range(len(self.runes_json[i]['slots'][j]['runes']))
+                ]
+                for j in range(len(self.runes_json[i]['slots']))
+            ]
+            for i in range(len(self.runes_json))
+        ]
+        self.boots                  = [
+            e for e in self.items
+            if 'Boots' in self.items[e]['tags']
+            and not 'into' in self.items[e]
+            and self.items[e]['maps']['11']
+        ]
+        self.mythics        = [
+            e for e in self.items
+            if 'rarityMythic' in self.items[e]['description']
+            and not e in self.boots
+            and self.items[e]['maps']['11']
+        ]
+        self.legendary      = [
+            e for e in self.items
+            if not 'rarityMythic' in self.items[e]['description']
+            and not 'into' in self.items[e]
+            and self.items[e]['gold']['base'] > 500
+            and not e in self.boots
+            and self.items[e]['maps']['11']
+        ]
 
-runes_pages = [path for path in RUNES[0]]
-pre_runes = [randint(0, len(runes_pages) - 1) for _ in range(2)]
-while pre_runes[0] == pre_runes[1]:
-    pre_runes[1] = randint(0, len(runes_pages) - 1)
+        print(self)
 
-runes_paths = [
-    RUNES[0][runes_pages[pre_runes[0]]],
-    RUNES[0][runes_pages[pre_runes[1]]]
-]
+    def generate_pick(self):
+        champion    = self.champions[randint(0, len(self.champions) - 1)]
+        position    = self.positions[randint(0, len(self.positions) - 1)]
+        mythic      = self.items[self.mythics[randint(0, len(self.mythics) - 1)]]['name']
 
-runes = [
-    [
-        [runes_paths[0][i][randint(0, len(runes_paths[0][i]) - 1)] for i in range(4)],
-        [runes_paths[1][j + 1 + (pre_runes[0] + pre_runes[1]) // 4]
-            [randint(0, len(runes_paths[1][j + 1 + (pre_runes[0] + pre_runes[1]) // 4]) - 1)] for j in range(2)]
-    ],
-    [RUNES[1][f"Slot{slot + 1}"][randint(0, 2)] for slot in range(3)]
-]
+        main_branch = randint(0, len(self.runes) - 1)
+        secondary_branch = randint(0, len(self.runes) - 1)
+        while secondary_branch == main_branch:
+            secondary_branch = randint(0, len(self.runes) - 1)
 
-#################################
-#   BUILD
-#################################
+        runes = [
+            [
+                self.runes[main_branch][i][randint(
+                    0, len(self.runes[main_branch][i]) - 1)]
+                for i in range(4)
+            ],
+            [
+                self.runes[secondary_branch][j][randint(
+                    0, len(self.runes[secondary_branch][j]) - 1)]
+                for j in range(1, 3)
+            ]
+        ]
 
-len_finished_items = len(FINISHED_ITEMS)
+        spells      = 'QWE'[randint(0, 2)]
+        for _ in range(2):
+            l = 'QWE'[randint(0, 2)]
+            while l in spells: l = 'QWE'[randint(0, 2)]
+            spells += l
 
-if role == "Support": build = [SUPPORT_ITEMS[randint(0, len(SUPPORT_ITEMS) - 1)]]
-elif role == "Jungle":
-    core_items = ["Skirmisher's Sabre", "Stalker's Blade"]
-    enchant_items = ["Bloodrazor", "Cinderhulk", "Runic Echoes", "Warrior"]
-    build = [f"{core_items[randint(0, len(core_items) - 1)]} ({enchant_items[randint(0, len(enchant_items) - 1)]})"]
-else:
-    build = [FINISHED_ITEMS[randint(0, len_finished_items - 1)]]
+        if position == 'Jungle':
+            summoner_spells = [self.summoner_spells[randint(0, len(self.summoner_spells) - 1)], 'Smite']
+        else:
+            summoner_spells = [self.summoner_spells[randint(0, len(self.summoner_spells) - 1)] for _ in range(2)]
 
-while len(build) < 5:
-    new_item = FINISHED_ITEMS[randint(0, len_finished_items - 1)]
-    if not new_item in build:
-        build.append(new_item)
+        if champion == 'Cassiopeia':
+            legendaries = [self.items[self.legendary[randint(0, len(self.legendary) - 1)]]['name'] for _ in range(6)]
+        else:
+            legendaries = [self.items[self.legendary[randint(0, len(self.legendary) - 1)]]['name'] for _ in range(5)]
+            boots       = self.items[self.boots[randint(0, len(self.boots) - 1)]]['name']
 
-#################################
-#   SUMMONER SPELLS
-#################################
+        r = f"""
+        CHAMPION:\t{champion}
+        POSITION:\t{position}
+        MYTHIC:\t\t{mythic}
+        LEGENDARY:\t{legendaries}
+        SUMMONERS:\t{summoner_spells[0]}, {summoner_spells[1]}
+        SPELLS:\t\t{spells[0]} > {spells[1]} > {spells[2]}
+        RUNES:\t\t{runes}
+        """
 
-len_summoner_spells = len(SUMMONER_SPELLS)
+        if champion != 'Cassiopeia': r += f'BOOTS:\t\t{boots}\n'
+        return r
 
-if role == "Jungle":
-    sums = ["Smite"]
-else: sums = [SUMMONER_SPELLS[randint(0, len_summoner_spells - 1)]]
+    def __str__(self):
+        return self.generate_pick()
 
-while len(sums) < 2:
-    new_sums = SUMMONER_SPELLS[randint(0, len_summoner_spells - 1)]
-    if not new_sums in sums:
-        sums.append(new_sums)
-
-#################################
-#   SPELLS ORDER
-#################################
-
-while len(spells) < 3:
-    new_spell = 'QWE'[randint(0, 2)]
-    if not new_spell in spells:
-        spells.append(new_spell)
-
-#################################
-#   DISPLAY
-#################################
-
-print(f"""
-ROLE: {role}
-CHAMPION: {champion}
-SUMMONERS: {sums[0]}, {sums[1]}
-RUNES: ({runes_pages[pre_runes[0]]}, {runes_pages[pre_runes[1]]})
-\t- {runes[0][0][0]} : {runes[0][0][1]} - {runes[0][0][2]} - {runes[0][0][3]}
-\t- {runes[0][1][0]} - {runes[0][1][1]}
-\t- {runes[1][0]} - {runes[1][1]} - {runes[1][2]}
-
-SPELLS: {spells[0]} > {spells[1]} > {spells[2]}
-BUILD: {build}
-BOOTS: {boots}
-""")
-
-input("Good luck, have fun !")
+pick = Scraper()
+while input("Re-roll? [y/n]: ")[0].lower() == 'y':
+    print(pick)
