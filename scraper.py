901d82efc83f18c6f864ab8004f25a53d3e8fa33
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2021-01-18 20:28:41 -0500

    Made the code easier to read

--- scraper.py
+++ scraper.py
@@ -7,61 +7,98 @@ import requests
 import json
 
 class Scraper:
-    def __init__(self):
+    def __init__(self, region='en_US'):
         '''
         Since League of Legends is developed by Riot Games
         This program obtains its data from Riot Games services.
         '''
+        self.base_url   = 'https://ddragon.leagueoflegends.com'
+        self.region     = region
+
         try:
-            self.base_url = 'https://ddragon.leagueoflegends.com'
-            self.region = 'en_US'
-            self.patch = requests.get(f'{self.base_url}/api/versions.json').json()[0]
-            self.champions_json = requests.get(f'{self.base_url}/cdn/{self.patch}/data/{self.region}/champion.json').json()
-            self.items_json = requests.get(f'{self.base_url}/cdn/{self.patch}/data/{self.region}/item.json').json()
-            self.summoner_spells_json = requests.get(f'{self.base_url}/cdn/{self.patch}/data/{self.region}/summoner.json').json()
-            self.runes_json = requests.get(f'{self.base_url}/cdn/{self.patch}/data/{self.region}/runesReforged.json').json()
+            # Get the data online
+            self.patch                  = self.get_patch()
+            self.champions_json         = self.get_champions_json()
+            self.items_json             = self.get_items_json()
+            self.summoner_spells_json   = self.get_summoner_spells_json()
+            self.runes_json             = self.get_runes_json()
 
-            self.champions = [champion for champion in self.champions_json['data']]
-            for i, n in enumerate(self.champions[74:]):
-                if n == 'MonkeyKing': self.champions[74 + i] = 'Wukong'
-
-            self.items = self.items_json['data']
-            self.positions = ['Top', 'Jungle',
-                            'Middle', 'Bottom (ADC/APC)', 'Support']
-            self.summoner_spells = [self.summoner_spells_json['data'][e]['name']
-                                    for e in self.summoner_spells_json['data'] if 'CLASSIC' in self.summoner_spells_json['data'][e]['modes']]
-            self.runes = [
-                [
-                    [
-                        self.runes_json[i]['slots'][j]['runes'][k]['key']
-                        for k in range(len(self.runes_json[i]['slots'][j]['runes']))
-                    ]
-                    for j in range(len(self.runes_json[i]['slots']))
-                ]
-                for i in range(len(self.runes_json))
-            ]
-            self.boots = [
-                e for e in self.items
-                if 'Boots' in self.items[e]['tags']
-                and not 'into' in self.items[e]
-                and self.items[e]['maps']['11']
-            ]
-            self.mythics = [
-                e for e in self.items
-                if 'rarityMythic' in self.items[e]['description']
-                and not e in self.boots
-                and self.items[e]['maps']['11']
-            ]
-            self.legendary = [
-                e for e in self.items
-                if not 'rarityMythic' in self.items[e]['description']
-                and not 'into' in self.items[e]
-                and self.items[e]['gold']['base'] > 500
-                and not e in self.boots
-                and self.items[e]['maps']['11']
-            ]
+            # Read the data
+            self.champions              = self.load_champions()
+            self.items                  = self.items_json['data']
+            self.positions              = ['Top', 'Jungle', 'Middle', 'Bottom (ADC/APC)', 'Support']
+            self.summoner_spells        = self.load_summoner_spells()
+            self.runes                  = self.load_runes()
+            self.boots                  = self.load_boots()
+            self.mythics                = self.load_mythics()
+            self.legendary              = self.load_legendary()
         except:
             raise ValueError('Unable to load data!')
+
+    def get_patch(self):
+        return requests.get(f'{self.base_url}/api/versions.json').json()[0]
+
+    def get_champions_json(self):
+        return requests.get(f'{self.base_url}/cdn/{self.patch}/data/{self.region}/champion.json').json()
+
+    def get_items_json(self):
+        return requests.get(f'{self.base_url}/cdn/{self.patch}/data/{self.region}/item.json').json()
+
+    def get_summoner_spells_json(self):
+        return requests.get(f'{self.base_url}/cdn/{self.patch}/data/{self.region}/summoner.json').json()
+
+    def get_runes_json(self):
+        return requests.get(f'{self.base_url}/cdn/{self.patch}/data/{self.region}/runesReforged.json').json()
+
+    def load_champions(self):
+        champions = [champion for champion in self.champions_json['data']]
+        for i, n in enumerate(champions[74:]):
+            if n == 'MonkeyKing': champions[74 + i] = 'Wukong'
+        return champions
+
+    def load_summoner_spells(self):
+        return [
+            self.summoner_spells_json['data'][e]['name']
+            for e in self.summoner_spells_json['data'] if 'CLASSIC' in self.summoner_spells_json['data'][e]['modes']
+        ]
+
+    def load_boots(self):
+        return [
+            e for e in self.items
+            if 'Boots' in self.items[e]['tags']
+            and not 'into' in self.items[e]
+            and self.items[e]['maps']['11']
+        ]
+
+    def load_mythics(self):
+        return [
+            e for e in self.items
+            if 'rarityMythic' in self.items[e]['description']
+            and not e in self.boots
+            and self.items[e]['maps']['11']
+        ]
+
+    def load_legendary(self):
+        return [
+            e for e in self.items
+            if not 'rarityMythic' in self.items[e]['description']
+            and not 'into' in self.items[e]
+            and self.items[e]['gold']['base'] > 500
+            and not e in self.boots
+            and self.items[e]['maps']['11']
+        ]
+
+    def load_runes(self):
+        return [
+            [
+                [
+                    self.runes_json[i]['slots'][j]['runes'][k]['key']
+                    for k in range(len(self.runes_json[i]['slots'][j]['runes']))
+                ]
+                for j in range(len(self.runes_json[i]['slots']))
+            ]
+            for i in range(len(self.runes_json))
+        ]
 
     def generate_pick(self):
         # Champion <Aatrox:...:Zyra>
@@ -99,6 +136,7 @@ class Scraper:
                 l = 'QWE'[randint(0, 2)]
             spells += l
 
+        # Summoner spells
         if position == 'Jungle':
             summoner_spells = [self.summoner_spells[randint(0, len(self.summoner_spells) - 1)], 'Smite']
         else:
@@ -106,29 +144,24 @@ class Scraper:
         while summoner_spells[0] == summoner_spells[1]:
             summoner_spells[1] = self.summoner_spells[randint(0, len(self.summoner_spells) - 1)]
 
+        # Legendary items
+        legendaries = [self.items[self.legendary[randint(0, len(self.legendary) - 1)]]['name']]
+        while len(legendaries) < 4:
+            new_item = self.items[self.legendary[randint(0, len(self.legendary) - 1)]]['name']
+            if new_item not in legendaries:
+                legendaries.append(new_item)
+
+        # Boots
         if champion == 'Cassiopeia':
-            legendaries = [self.items[self.legendary[randint(0, len(self.legendary) - 1)]]['name']]
             while len(legendaries) < 5:
                 new_item = self.items[self.legendary[randint(0, len(self.legendary) - 1)]]['name']
                 if new_item not in legendaries:
                     legendaries.append(new_item)
         else:
-            legendaries = [self.items[self.legendary[randint(0, len(self.legendary) - 1)]]['name']]
-            while len(legendaries) < 4:
-                new_item = self.items[self.legendary[randint(0, len(self.legendary) - 1)]]['name']
-                if new_item not in legendaries:
-                    legendaries.append(new_item)
             boots = self.items[self.boots[randint(0, len(self.boots) - 1)]]['name']
 
-        r = [
-            champion,
-            position,
-            mythic,
-            legendaries,
-            summoner_spells,
-            spells,
-            runes
+        return [
+            champion, position, mythic, legendaries, summoner_spells, spells, runes, boots
+        ] if champion != 'Cassiopeia' else [
+            champion, position, mythic, legendaries, summoner_spells, spells, runes
         ]
-        if champion != 'Cassiopeia':
-            r.append(boots)
-        return r
